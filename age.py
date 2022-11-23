070aabde4b581d1958d85b9e2d00eb7b51aa5990
Leandro Caetano <lcaetano30@gmail.com>
2022-11-23 00:09:43 -0300

    age_counting

---
+++ age.py
@@ -0,0 +1,12 @@
+import requests
+
+result = requests.get("https://coderbyte.com/api/challenges/json/age-counting")
+
+resp_data = result.json()["data"].split(",")
+
+count = 0
+for data in resp_data:
+    split_data = data.split("=")
+    if split_data[0].strip() == "age" and int(split_data[1]) >= 50:
+        count +=1
+print(count)
